65daf6b33320b8fb16b5f630515f03eed80cb446
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-22 13:16:08 +0530

    Added:The number on dice is randomly generated

--- SnakeLadder/src/Main.java
+++ SnakeLadder/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		int pos = 0;
 		System.out.println("The player is starting at position "+pos);
+		RollingDice dice = new RollingDice();
+		System.out.println("The number on dice is "+dice.Roll());
 	}
 }
